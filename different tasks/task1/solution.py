0550bcb527aae61c2d0c0147bbce5736842bcd59
Andrey1310Dov <40867514+Andrey1310Dov@users.noreply.github.com>
2019-03-26 11:16:06 +0300

    Update solution.py

--- different tasks/task1/solution.py
+++ different tasks/task1/solution.py
@@ -13,4 +13,24 @@ def search_pairs(array, k):
 print(search_pairs([1, 2, 6, 5, 3, 4, 7, 8, 3, 2], 5))
 
 # Сложность алгоритма O(n^2)
+
+def search_pairs(array, k):
+    check_second_digit = set(array)
+    check_pairs = set()
+    out = []
+    for i in array:
+        j = k - i
+        if j in check_second_digit:
+            if j not in check_pairs:
+                out.append((i, j))
+                check_pairs.add(i)
+                check_pairs.add(j)
+    return list(out)
+   
+print(search_pairs([1, 2, 6, 5, 3, 4, 7, 8, 3, 2, 8, 3, 4, 7, 5, 2, 7, 8, 0], 6))
+print(search_pairs([1, 2, 6, 5, 3, 4, 7, 8, 3, 2], 5))
+print(search_pairs([1, 2, 6, 5, 3, 4, 7, 8, 3, 2, 8, 3, 4, 7, 5, 2, 7, 8, 0], 3))
+print(search_pairs([4, 4, 4, 2, 4, 2, 6, 7, 6, 2], 8))
+
+# Сложность алгоритма O(n)
    
